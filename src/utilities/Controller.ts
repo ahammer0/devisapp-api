ba24ea9c7a6d89064d1353dbba1863d8d3967006
ahammer0 <axel.schwindenhammer@gmail.com>
2024-12-18 18:47:38 +0100

    add dev/prod logging of errors

--- src/utilities/Controller.ts
+++ src/utilities/Controller.ts
@@ -1,22 +1,37 @@
-import {Connection} from "promise-mysql";
-import ErrorResponse from "./ErrorResponse";  
-import {Response} from "express";
+import { Connection } from "promise-mysql";
+import ErrorResponse from "./ErrorResponse";
+import { Response } from "express";
 
 export default abstract class Controller {
-  db: Connection
+  db: Connection;
   constructor(db: Connection) {
     this.db = db;
   }
-  static handleError(err: any,res:Response):void {
+  static handleError(err: any, res: Response): void {
+    const env = process.env.ENV;
+    if (env === undefined || !(env === "dev" || env === "prod")) {
+      throw new Error(
+        "ENV is not defined in .env file, it must be dev or prod"
+      );
+    }
+
     if (err instanceof ErrorResponse) {
       res.status(err.code).json({ message: err.message });
+      if (env === "dev") console.error(err.message);
       return;
     }
     if (typeof err === "string") {
       res.status(500).json({ message: err });
       return;
     }
-    res.status(500).json({ message: "Internal server error" });
-    return 
+
+    if (env === "dev") {
+      console.error(err);
+      res.status(500).json({ err });
+      return;
+    } else {
+      res.status(500).json({ message: "Internal server error" });
+      return;
+    }
   }
 }
